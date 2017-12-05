3aa5e94269a74606a1806589b82c8c642e1a8a19
Matthew Kressin <matthew.d.kressin@gmail.com>
2017-12-04 21:36:42 -0800

    testing depth-first search
    changed DepthFirstSearch to be its own class instead of an inner class
    for AbstractPokerGraph.
    added beginning of a JUnit tester

---
+++ Casino/src/edu/miracosta/cs113/DepthFirstSearch.java
@@ -0,0 +1,106 @@
+package edu.miracosta.cs113;
+
+import java.util.Iterator;
+
+/**
+ * Class to implement the depth-first search algorithm
+ */
+public class DepthFirstSearch
+{
+	// Data Fields
+	/** A reference to the graph being searched. */
+	Graph graph;
+	/** Array of parents in the depth-first search. */
+	private int[] parent;
+	/** Flag to indicate whether this vertex has been visited. */
+	private boolean[] visited;
+	/** The array that contains each vertex in discover order. */
+	private int[] discoveryOrder;
+	/** The array that contains each vertex in finish order. */
+	private int[] finishOrder;
+	/** The index that indicates the discovery order. */
+	private int discoverIndex = 0;
+	/** The index that indicates the finish order. */
+	private int finishIndex = 0;
+	
+	/**
+	 * Construct the depth-first search of a Graph starting at vertex 0 and
+	 * visiting the start vertices in ascending order.
+	 * @param graph	The graph
+	 */
+	public DepthFirstSearch(Graph graph) 
+	{
+		this.graph = graph;
+		int n = graph.getNumV();
+		parent = new int[n];
+		visited = new boolean[n];
+		discoveryOrder = new int[n];
+		finishOrder = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			parent[i] = -1;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			if (!visited[i])
+			{
+				depthFirstSearch(i);
+			}
+		}
+	}
+	/**
+	 * Construct the depth-first search of a Graph selecting the start vertices
+	 * in the specified order. The first vertex visited is order[0]
+	 * @param graph	The graph
+	 * @param order	The array giving the order in which the start vertices
+	 * 				should be selected
+	 */
+	public DepthFirstSearch(Graph graph, int[] order)
+	{
+		this.graph = graph;
+		int n = graph.getNumV();
+		parent = new int[n];
+		visited = new boolean[n];
+		discoveryOrder = new int[n];
+		finishOrder = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			parent[i] = -1;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			if (!visited[order[i]])
+			{
+				depthFirstSearch(order[i]);
+			}
+		}
+	}
+	
+	/**
+	 * Recursively depth-first search the graph starting at vertex current.
+	 * @param current	The start vertex
+	 */
+	public void depthFirstSearch(int current)
+	{
+		/* Mark the current vertex visited. */
+		visited[current] = true;
+		discoveryOrder[discoverIndex++] = current;
+		/* Examine each vertex adjacent to the current index. */
+		Iterator<Edge> itr = graph.edgeIterator(current);
+		while (itr.hasNext())
+		{
+			int neighbor = itr.next().getDest();
+			/* Process a neighbor that has not been visited */
+			if (!visited[neighbor])
+			{
+				/* Insert (current, neighbor) into the depth-first 
+				 * search tree. */
+				parent[neighbor] = current;
+				/* Recursively apply the algorithm starting at neighbor. */
+				depthFirstSearch(neighbor);
+			}
+		}
+		/* Mark current finished. */ 
+		finishOrder[finishIndex++] = current;
+	}
+} // end of DepthFirstSearch inner class
---
+++ Casino/src/edu/miracosta/cs113/DepthFirstSearchTest.java
@@ -0,0 +1,22 @@
+package edu.miracosta.cs113;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Algorithm for testing search traversal methods of the graphs
+ * 1.	test depth-first search
+ * 2.		instantiate ListPokerGraph object
+ * 3.			insert edges to create graph
+ * 4.			use depth-first search
+ * 5.			compare result with expected result
+ *
+ */
+public class DepthFirstSearchTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
