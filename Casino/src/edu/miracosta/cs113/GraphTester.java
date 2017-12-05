da9c079e8d90f6ea37bad6c041884e72420d3b0a
Matthew Kressin <matthew.d.kressin@gmail.com>
2017-12-04 19:57:27 -0800

    testing isEdge for ListPokerGraph
    testing different cases for isEdge for an undirected ListPokerGraph

--- Casino/src/edu/miracosta/cs113/GraphTester.java
+++ Casino/src/edu/miracosta/cs113/GraphTester.java
@@ -22,6 +22,13 @@ import org.junit.Test;
  * 15.			getEdges from graph and check to see if they're correct
  * 16.		test method getEdge in MatrixPokerGraph
  * 17.			repeat steps 13-15 for MatrixPokerGraph
+ * 18.	test method isEdge
+ * 19.		test method isEdge in ListPokerGraph
+ * 20.			instantiate ListPokerGraph object
+ * 21.			insert Edges into graph
+ * 22.			check with edges in graph and edges not in the graph
+ * 23.		test method isEdge in MatrixPokerGraph
+ * 24.			repeat steps 20-22 for MatrixPokerGraph
  *
  */
 public class GraphTester {
@@ -82,6 +89,28 @@ public class GraphTester {
 		Edge e2 = list.getEdge(edge2.getSource(), edge2.getDest());
 		Edge e3 = list.getEdge(edge3.getSource(), edge3.getDest());
 		Assert.assertTrue(edge2.equals(e2) && edge3.equals(e3));
+	}
+	@Test
+	public void testListPokerGraphIsEdge()
+	{
+		AbstractPokerGraph list = new ListPokerGraph(7,false);
+		Edge edge1 = new Edge(0,2);
+		Edge edge2 = new Edge(2,4);
+		Edge edge3 = new Edge(4,6);
+		
+		list.insert(edge1);
+		list.insert(edge2);
+		list.insert(edge3);
+		
+		Assert.assertTrue(((ListPokerGraph) list).isEdge(0, 2));
+		Assert.assertTrue(((ListPokerGraph) list).isEdge(2, 4));
+		Assert.assertTrue(((ListPokerGraph) list).isEdge(4, 6));
+		Assert.assertTrue(((ListPokerGraph) list).isEdge(6, 4));//undirected graph
+		
+		// test for edges that are not in the graph
+		Assert.assertFalse(((ListPokerGraph) list).isEdge(0, 6));
+		Assert.assertFalse(((ListPokerGraph) list).isEdge(2, 6));
+		Assert.assertFalse(((ListPokerGraph) list).isEdge(0, 4));
 	}
 	@Test
 	public void testCreateGraph() {
