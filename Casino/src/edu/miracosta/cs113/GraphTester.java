a0641d8e9126e943054dc1cbc3ffc805dcb6ba6d
Matthew Kressin <matthew.d.kressin@gmail.com>
2017-12-04 19:30:42 -0800

    testing insert() for ListPokerGraph
    testing method insert with different cases in JUnit

--- Casino/src/edu/miracosta/cs113/GraphTester.java
+++ Casino/src/edu/miracosta/cs113/GraphTester.java
@@ -10,6 +10,12 @@ import org.junit.Test;
  * 3.		test constructor of MatrixPokerGraph
  * 4.	test method insert
  * 5.		test method insert in ListPokerGraph
+ * 6.			instantiate ListPokerGraph object
+ * 7.			instantiate Edge objects
+ * 8.			insert Edge objects into the ListPokerGraph
+ * 9.		test method insert in MatrixPokerGraph
+ * 10.			repeat steps 6-8 for MatrixPokerGraph
+ * 11.
  *
  */
 public class GraphTester {
@@ -31,6 +37,27 @@ public class GraphTester {
 		int vertices = matrix.getNumV();
 		Assert.assertTrue(vertices == 7);
 		Assert.assertFalse(matrix.isDirected());
+	}
+	@Test
+	public void testListPokerGraphInsert()
+	{
+		AbstractPokerGraph list = new ListPokerGraph(7,false);
+		Edge edge1 = new Edge(0,2);
+		Edge edge2 = new Edge(2,4);
+		Edge edge3 = new Edge(4,6);
+		
+		list.insert(edge1);
+		Assert.assertTrue(((ListPokerGraph) list).isEdge(0,2));
+		Assert.assertTrue(((ListPokerGraph) list).getEdge(0, 2).equals(edge1));
+		// test with edge not in list
+		Assert.assertFalse(((ListPokerGraph) list).isEdge(2, 4));
+		
+		list.insert(edge2);
+		list.insert(edge3);
+		Assert.assertTrue(((ListPokerGraph) list).isEdge(2,4));
+		Assert.assertTrue(((ListPokerGraph) list).getEdge(2, 4).equals(edge2));
+		Assert.assertTrue(((ListPokerGraph) list).isEdge(4,6));
+		Assert.assertTrue(((ListPokerGraph) list).getEdge(4, 6).equals(edge3));	
 	}
 	@Test
 	public void testCreateGraph() {
