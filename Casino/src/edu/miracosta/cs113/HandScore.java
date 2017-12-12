1eef893334ede62ebd40d71460bb7a25266f3841
Eric Thompson <ET1483@gmail.com>
2017-12-11 21:16:45 -0800

    Working on finding total score of graph

--- Casino/src/edu/miracosta/cs113/HandScore.java
+++ Casino/src/edu/miracosta/cs113/HandScore.java
@@ -59,6 +59,7 @@ public class HandScore
 		int[] dOrder = dfs.getDiscoveryOrder();
 		
 		ListPokerGraph<Hand> list = (ListPokerGraph<Hand>) possibleHands;
+		
 		for (int i = 0; i < list.getNumV(); i++)
 		{
 			System.out.println("IN LOOP");
